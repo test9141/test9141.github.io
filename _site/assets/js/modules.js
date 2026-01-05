0a7e6ce465057224e942fa4def6e3ab6d4f1dbc5
script26 <97182340+script26@users.noreply.github.com>
2026-01-04 22:32:42 -0700

    Update modules.js

--- _site/assets/js/modules.js
+++ _site/assets/js/modules.js
@@ -2,12 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    higherCharLimit();
-    colorPicker();
-    unicodeRemoval();
-    keybindings();
-    userBlocking();
-    colorWheel();
     backgrounds();
     gifAutoload();
     buttons();
